831684431f6f59f71d87119215dc393e95788d7d
Khrystyna <khrystynadyka@gmail.com>
2016-11-16 10:23:59 +0000

    add header

--- js/ordering.js
+++ js/ordering.js
@@ -1,29 +1,35 @@
 $(function() {
     $('#submit').click(function(e) {
-      e.preventDefault(); 
-      $.ajax({
-    url: "https://formspree.io/dykakh@gmail.com", 
-    method: "POST",
-    data: {
-        userName: $('#userName').val(),
-        phoneNumber: $('#phoneNumber').val(),
-        userEmail: $('#userEmail').val(),
-        doneDate: $('#doneDate').val(),
-        cake: $('#cake').val(),
-        cupcake: $('#cupcake').val(),
-        comment: $('#comment').val()
-    },
-        
-        
-    dataType: "json"
-    
-})
-    .done(function() {
-        $('form').html('<h1>Ваше замовлення прийняте!</h1>');
-    })
-    .fail(function() {
-        $('form').html('<h1>Ой, щось сталось =(</h1>');
+        if ($('#orderingForm')[0].checkValidity()) {
+            var cake = 'not ordered';
+            var cupcake = 'not ordered';
+            if ($('#cake').is(":checked")) {
+                cake = $('#cake').val();
+            }
+            if ($('#cupcake').is(":checked")) {
+                cupcake = $('#cupcake').val();
+            }
+            e.preventDefault();
+            $.ajax({
+                    url: "https://formspree.io/dykakh@gmail.com",
+                    method: "POST",
+                    data: {
+                        userName: $('#userName').val(),
+                        phoneNumber: $('#phoneNumber').val(),
+                        userEmail: $('#userEmail').val(),
+                        doneDate: $('#doneDate').val(),
+                        cake: cake,
+                        cupcake: cupcake,
+                        comment: $('#comment').val()
+                    },
+                    dataType: "json"
+                })
+                .done(function() {
+                    $('form').html('<h1>Ваше замовлення прийняте!</h1>');
+                })
+                .fail(function() {
+                    $('form').html('<h1>Ой, щось сталось =(</h1>');
+                });
+        }
     });
-        
-    });
-}); 
+});
